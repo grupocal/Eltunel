854c8cbf71c976d97020eaab8a6ae4844e47a2f4
jmgr99 <62746568+jmgr99@users.noreply.github.com>
2020-06-08 17:07:20 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-
+function myFunction() {
+	var aa1 = document.getElementById("aa1");
+	if (aa1.style.display === "none") {
+	  aa1.style.display = "block";
+	} else {
+	  aa1.style.display = "none";
+	}
+	}
