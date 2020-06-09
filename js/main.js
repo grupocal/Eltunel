5c97020fe2db68401634a476a4c3572675f64b39
jmgr99 <62746568+jmgr99@users.noreply.github.com>
2020-06-09 18:08:20 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,12 +4,28 @@ function encode_utf8(s) {
 function decode_utf8(s) {
   return decodeURIComponent(escape(s));
 }
-var sheva = decode_utf8('\xD6\xB0');
 
-var conjdict = {"Isg":sheva}
+var rad1 = "ק"
+var rad2 = "ט"
+var rad3 = "ל"
+
+var sheva = decode_utf8('\xD6\xB0');
+var hatefsegol = decode_utf8('\xD6\xB1');
+var hatefpataj = decode_utf8('\xD6\xB2\x20');
+var hatefqamets = decode_utf8('\xD6\xB3\x20');
+var hireq = decode_utf8('\xD6\xB4');
+var tsere = decode_utf8('\xD6\xB5');
+var segol = decode_utf8('\xD6\xB6');
+var pataj = decode_utf8('\xD6\xB7');
+var qamets = decode_utf8('\xD6\xB8');
+var holem = decode_utf8('\xD6\xB9');
+var qubuts = decode_utf8('\xD6\xBB\x20');
+var daguesh = decode_utf8('\xD6\xBC');
+
+var conjdict = {"Isg":[sheva,rad1]}
 
 function myFunction() {
-	var a1 = document.getElementById("a1").innerHTML = conjdict["Isg"];
+	var a1 = document.getElementById("a1").innerHTML = conjdict["Isg"].join();
 	var aa1 = document.getElementById("aa1");
 	
 	if (aa1.style.display === "none") {
