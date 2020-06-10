b5f8cd838e99ffed8f1e23c8231a543ee6c375de
jmgr99 <62746568+jmgr99@users.noreply.github.com>
2020-06-10 17:15:12 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -23,33 +23,55 @@ var qubuts = decode_utf8('\xD6\xBB\x20');
 var daguesh = decode_utf8('\xD6\xBC');
 
 var conjdict = {'qal':
-			{'perfect':
-				{"Isg":[rad1,qamets,rad2,pataj,rad3,sheva,"ת",daguesh,hireq,"י"],
-"IIsgm":[rad1,qamets,rad2,pataj,rad3,sheva,"ת",daguesh,qamets],
-"IIsgf":[rad1,qamets,rad2,pataj,rad3,sheva,"ת",daguesh,sheva],
-"IIIsgm":[rad1,qamets,rad2,pataj,rad3],
-"IIIsgf":[rad1,qamets,rad2,sheva,rad3,qamets,"ה"],
-"Ipl":[rad1,qamets,rad2,pataj,rad3,sheva,"נו",daguesh],
-"IIplm":[rad1,sheva,rad2,pataj,rad3,sheva,"ת",daguesh,segol,"ם"],
-"IIplf":[rad1,sheva,rad2,pataj,rad3,sheva,"ת",daguesh,segol,"ן"],
-"IIIplm":[rad1,qamets,rad2,sheva,rad3,"ו",daguesh],
-"IIIplf":[rad1,qamets,rad2,sheva,rad3,"ו",daguesh]}}}
+			           {'perfect':
+				             {"Isg":[rad1,qamets,rad2,pataj,rad3,sheva,"ת",daguesh,hireq,"י"],
+                      "IIsgm":[rad1,qamets,rad2,pataj,rad3,sheva,"ת",daguesh,qamets],
+                      "IIsgf":[rad1,qamets,rad2,pataj,rad3,sheva,"ת",daguesh,sheva],
+                      "IIIsgm":[rad1,qamets,rad2,pataj,rad3],
+                      "IIIsgf":[rad1,qamets,rad2,sheva,rad3,qamets,"ה"],
+                      "Ipl":[rad1,qamets,rad2,pataj,rad3,sheva,"נו",daguesh],
+                      "IIplm":[rad1,sheva,rad2,pataj,rad3,sheva,"ת",daguesh,segol,"ם"],
+                      "IIplf":[rad1,sheva,rad2,pataj,rad3,sheva,"ת",daguesh,segol,"ן"],
+                      "IIIplm":[rad1,qamets,rad2,sheva,rad3,"ו",daguesh],
+                      "IIIplf":[rad1,qamets,rad2,sheva,rad3,"ו",daguesh]}
+                  {'imperfect':
+                      "Isg":["א",segol,rad1,sheva,rad2,holem,rad3],
+                      "IIsgm":["ת",daguesh,hireq,rad1,sheva,rad2,holem,rad3],
+                      "IIsgf":["ת",daguesh,hireq,rad1,sheva,rad2,sheva,rad3,hireq,"י"],
+                      "IIIsgm":["י",hireq,rad1,sheva,rad2,holem,rad3,],
+                      "IIIsgf":["ת",daguesh,hireq,rad1,sheva,rad2,holem,rad3],
+                      "Ipl":["נ",hireq,rad1,sheva,rad2,holem,rad3],
+                      "IIplm":["ת",daguesh,hireq,rad1,sheva,rad2,sheva,rad3,"ו",daguesh],
+                      "IIplf":["ת",daguesh,hireq,rad1,sheva,rad2,holem,rad3,sheva,"נ",qamets,"ה"],
+                      "IIIplm":["י",hireq,rad1,sheva,rad2,sheva,rad3,"ו",daguesh],
+                      "IIIplf":["ת",daguesh,hireq,rad1,sheva,rad2,holem,rad3,sheva,"נ",qamets,"ה"]}
+                  {'participle':
+                       'mascsg': [rad1,"ו",holem,rad2,tsere,rad3]
+                       'femsg':  [rad1,"ו",holem,rad2,segol,rad3,segol,"ת"]
+                       'mascpl': [rad1,"ו",holem,rad2,sheva,rad3,hireq,"ים"]
+                       'fempl':  [rad1,"ו",holem,rad2,sheva,rad3,"ו",holem,"ת"]}
+                  {'infinitive':
+                       'absolute':[rad1,qamets,rad2,"ו",holem,rad3]
+                       'construct':["ל",hireq,rad1,sheva,rad2,holem,rad3]}
+                      }}
 
 
 				
 const person = ["Isg","IIsgm","IIsgf","IIIsgm","IIIsgf","Ipl","IIplm","IIplf","IIIplm","IIIplf"];
 
 
+
 function correct() {
-	const randomperson = person[Math.floor(Math.random() * person.length)];
+	var randomperson = function (conjdict) {
+    		var keys = Object.keys(conjdict);
+    		return conjdict[keys[ keys.length * Math.random() << 0]];};
 	var a1 = document.getElementById("a1").innerHTML = conjdict['qal']['perfect'][randomperson].join('');}
+
 function myFunction() {
 	var aa1 = document.getElementById("aa1");
-	
 	if (aa1.style.display === "none") {
 	  aa1.style.display = "block";
 	} else {
 	  aa1.style.display = "none";
 	}
-	
 	}
